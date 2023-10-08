0904a0fb27fc02e1981ee3be942b03109593cc09
MazharSolkar <mazharsolkar65@gmail.com>
2023-10-08 19:34:32 +0530

    todo with context api done

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import { TodoContext } from './contexts/TodoContext';
+
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
+
+function App() {
+  // Initialize todos state with localStorage data or an empty array
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem('todos')) || []
+  );
+
+  // Save todos to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (todo) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = [...prevTodos, { ...todo }];
+      return updatedTodos;
+    });
+  };
+
+  const updateTodo = (id, updatedTodo) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = prevTodos.map((prevtodo) =>
+        prevtodo.id === id ? { ...prevtodo, ...updatedTodo } : prevtodo
+      );
+      return updatedTodos;
+    });
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((prevtodo) => prevtodo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = prevTodos.map((prevtodo) =>
+        prevtodo.id === id
+          ? { ...prevtodo, completed: !prevtodo.completed }
+          : prevtodo
+      );
+      return updatedTodos;
+    });
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className='bg-[#172842] min-h-screen py-8'>
+        <div className='w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white'>
+          <h1 className='text-2xl font-bold text-center mb-8 mt-2'>
+            Manage Your Tasks
+          </h1>
+          <div className='mb-4'>
+            {/* Todo form goes here */}
+            <TodoForm />
+          </div>
+          <div className='flex flex-wrap gap-y-3'>
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className='w-full'>
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,56 @@
+import React, { useContext, useState } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
+
+function TodoItem({ todo }) {
+  const [isReadOnly, setIsReadOnly] = useState(true);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+
+  const { updateTodo, deleteTodo, toggleComplete } = useContext(TodoContext);
+
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsReadOnly(true);
+  };
+
+  return (
+    <div
+      className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+        todo.completed ? 'bg-[#c6e9a7]' : 'bg-[#ccbed7]'
+      }`}>
+      <input
+        type='checkbox'
+        className='cursor-pointer'
+        checked={todo.completed}
+        onChange={() => toggleComplete(todo.id)}
+      />
+      <input
+        className={`border outline-none w-full bg-transparent rounded-lg ${
+          isReadOnly ? 'border-transparent' : 'border-black/50 px-2'
+        } ${todo.completed ? 'line-through' : ''}`}
+        type='text'
+        value={todoMsg}
+        onChange={(e) => setTodoMsg(e.target.value)}
+        readOnly={isReadOnly}
+      />
+      {/* Edit, Save Button */}
+      <button
+        className='inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50'
+        onClick={() => {
+          if (todo.completed) return;
+
+          isReadOnly ? setIsReadOnly(!isReadOnly) : editTodo();
+        }}
+        disabled={todo.completed}>
+        {isReadOnly ? '✏️' : '📁'}
+      </button>
+      {/* Delete Todo Button */}
+      <button
+        className='inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0'
+        onClick={() => deleteTodo(todo.id)}>
+        ❌
+      </button>
+    </div>
+  );
+}
+
+export default TodoItem;
